115ac86a8c956446c5e7ca61d4ab684f5b3e0a29
Manish Kumar Bera <mkbera@cse.iitk.ac.in>
2018-11-16 23:22:01 +0530

    updated README

--- compile.py
+++ compile.py
@@ -1,22 +1,12 @@
 import os
 
-
-# compile_L1L2cache_LRU = 'gcc -O3 cache/L1L2cache_LRU.c -o executable/L1L2cache_LRU'
 compile_tasks = [
-	# 'gcc -O3 cache/L1L2cache_LRU.c -o executable/L1L2cache_LRU',
-	# 'gcc -O3 cache/LLC_MIN.c -o executable/LLC_MIN',
-	# 'gcc -O3 -w cache/LLC_MIN_meta.c -o executable/LLC_MIN_meta',
+	'gcc -O3 -w cache/L1L2cache_LRU.c -o executable/L1L2cache_LRU',
+	'gcc -O3 -w cache/LLC_MIN.c -o executable/LLC_MIN',
+	'gcc -O3 -w cache/LLC_MIN_meta.c -o executable/LLC_MIN_meta',
 	'gcc -O3 -w analysis/predictability.c -o executable/predictability -lm',
-	# 'gcc -O3 cache/L1cache_LRU.c -o executable/L1cache_LRU',
+	'gcc -O3 -w cache/L1cache_LRU.c -o executable/L1cache_LRU',
 ]
 
 for c in compile_tasks:	
 	os.system(c)
-
-# 6 218399787
-
-# 0 2198961740279
-# 0 2198961740281
-
-# *--0 2198961740281----0 2198961740281--*
-# *--0 2198961740281
